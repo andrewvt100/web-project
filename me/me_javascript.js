296e381189a951f02386efc50606be5680748d73
Andrew van Tonningen <andrewvt100@gmail.com>
2018-07-29 10:50:43 -0700

    test3

--- me/me_javascript.js
+++ me/me_javascript.js
@@ -2,7 +2,11 @@ main();
 
 function main() {
 	
-	
-			
+		$(document).ready(function(){
+	        $("#head-1").load("head.html");
+	        $("#footer-1").load("footer.html");
+	        $("#header-1").load("header.html");
+		});
+
 }
 
