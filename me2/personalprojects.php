4d80609e886fcfa7ba025232b58276a677c2547c
Andrew van Tonningen <andrewvt100@gmail.com>
2018-07-29 13:06:36 -0700

    updated projects page again

--- me2/personalprojects.php
+++ me2/personalprojects.php
@@ -11,13 +11,13 @@
 		<div class="in_text">
 			<p class="in_text_heading"> <a class = "header_link" href="../haunted/haunted_index.html"> Haunted Mansion </a> </p>
 			<p class="in_text_heading"> <a class = "header_link" href="../grateful/grateful_index.html"> What Are You Grateful For Today? </a> </p>
-			<p class="in_text_heading"> Spotify Power Hour App </p>
+			<p class="in_text_heading" color="#FF3B3F"> Spotify Power Hour App </p>
 			<p class="in_text_text">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fermentum velit imperdiet risus rutrum finibus. Morbi eu dictum felis, at sagittis ex. Maecenas at mauris eget sapien mollis viverra vitae ac nunc. Vestibulum porta vehicula dictum. In egestas posuere ipsum eu euismod. Maecenas eget bibendum velit. Vestibulum lobortis, tellus ut dictum finibus, mi mauris vestibulum justo, a sagittis tellus justo at lorem. Nulla facilisi. Pellentesque sed turpis eu metus elementum bibendum ac at nibh. Integer non pharetra orci. Nunc ultricies, orci quis iaculis pretium, quam sapien efficitur mi, nec sollicitudin orci nisl vitae leo. Cras auctor aliquet justo, sed euismod sapien tristique id. You can get it <a class="header_link" href="https://itunes.apple.com/us/app/simplelift-workout-tracker-and-lifting-app/id1083659710?mt=8"> here </a> on the app store.
+				A unreleased app that guarantees a perfect pregame.
 			</p>
-			<p class="in_text_heading"> SimpleLift </p>
+			<p class="in_text_heading" color="#FF3B3F"> SimpleLift </p>
 			<p class="in_text_text">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fermentum velit imperdiet risus rutrum finibus. Morbi eu dictum felis, at sagittis ex. Maecenas at mauris eget sapien mollis viverra vitae ac nunc. Vestibulum porta vehicula dictum. In egestas posuere ipsum eu euismod. Maecenas eget bibendum velit. Vestibulum lobortis, tellus ut dictum finibus, mi mauris vestibulum justo, a sagittis tellus justo at lorem. Nulla facilisi. Pellentesque sed turpis eu metus elementum bibendum ac at nibh. Integer non pharetra orci. Nunc ultricies, orci quis iaculis pretium, quam sapien efficitur mi, nec sollicitudin orci nisl vitae leo. Cras auctor aliquet justo, sed euismod sapien tristique id. You can get it <a class="header_link" href="https://itunes.apple.com/us/app/simplelift-workout-tracker-and-lifting-app/id1083659710?mt=8"> here </a> on the app store.
+				Developed by the team at Kadlecek LLC.  You can get it <a class="header_link" href="https://itunes.apple.com/us/app/simplelift-workout-tracker-and-lifting-app/id1083659710?mt=8"> here </a> on the app store.
 			</p>
 <!-- 			<p class="in_text_heading"> Warcraft 2 iOS Port </p>
 			<p class="in_text_text">
