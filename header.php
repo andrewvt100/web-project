82cb6dda675977bf694902c4fc1834f790de518b
Andrew van Tonningen <andrewvt100@gmail.com>
2018-07-29 14:05:34 -0700

    converted index html to php

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 		<a class="header_link" href="personalprojects.php"> Projects </a>
 	</div>
 	<div class="nav_tab">
-		<a class="header_link" href="aboutme.php"> About Me </a>
+		<a class="header_link" href="index.php"> About Me </a>
 	</div>
 	<div class="nav_tab">
 		<a class="header_link" href="workexperience.php"> Work Experience </a>
