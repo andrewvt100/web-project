cc35314ca252c03bc24c240f92714b102d4469d2
Andrew van Tonningen <andrewvt100@gmail.com>
2018-07-29 12:44:55 -0700

    added php version back in

---
+++ me2/head.php
@@ -0,0 +1,6 @@
+<title>
+	Andrew van Tonningen Work Portfolio
+</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
---
+++ me2/header.php
@@ -0,0 +1,21 @@
+<div class="name_section">
+	Andrew van Tonningen
+</div>
+<div class="separator_line">
+</div>
+<div class="buffer_zone">
+</div>
+<div class="tab_container">
+	<div class="nav_tab">
+		<a class="header_link" href="aboutme.php"> About Me </a>
+	</div>
+	<div class="nav_tab">
+		<a class="header_link" href="personalprojects.php"> Projects </a>
+	</div>
+	<div class="nav_tab">
+		<a class="header_link" href="workexperience.php"> Work Experience </a>
+	</div>
+	<div class="nav_tab">
+		<a class="header_link" href="contact.php"> Contact </a>
+	</div>
+</div>
