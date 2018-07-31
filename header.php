83b16ac2c9f41487cda87e2841d1911a43632ac4
Andrew van Tonningen <andrewvt100@gmail.com>
2018-07-31 15:12:47 +0000

    moved tabs up and right

--- header.php
+++ header.php
@@ -1,21 +1,25 @@
 <div class="name_section">
-	Andrew van Tonningen
+	<div class="tab_container">
+		<div class="nav_tab">
+			<a class="header_link" href="index.php"> About Me </a>
+		</div>
+		<div class="nav_tab">
+			<a class="header_link" href="personalprojects.php"> Projects </a>
+		</div>
+		<div class="nav_tab">
+			<a class="header_link" href="workexperience.php"> Work Experience </a>
+		</div>
+		<div class="nav_tab">
+			<a class="header_link" href="contact.php"> Contact </a>
+		</div>
+	</div>
+	<div class="logo_container">
+		Andrew van Tonningen
+	</div>
+</div>
+<div class="buffer_zone_a">
 </div>
 <div class="separator_line">
 </div>
-<div class="buffer_zone">
-</div>
-<div class="tab_container">
-	<div class="nav_tab">
-		<a class="header_link" href="index.php"> About Me </a>
-	</div>
-	<div class="nav_tab">
-		<a class="header_link" href="personalprojects.php"> Projects </a>
-	</div>
-	<div class="nav_tab">
-		<a class="header_link" href="workexperience.php"> Work Experience </a>
-	</div>
-	<div class="nav_tab">
-		<a class="header_link" href="contact.php"> Contact </a>
-	</div>
-</div>
+<div class="buffer_zone_b">
+</div> 
--- index.php
+++ index.php
@@ -7,15 +7,14 @@
 	<body class="whole_page">
 		<div id="wrap">
 			<div id="main">
-
-		<?php include("header.php"); ?>
-		<div class="in_text">
-			<p class="in_text_text">
-				I am currently a Software Application Engineer at Workday. In June 2018, I graduated from the University of California, Davis with a degree in Computer Science. During my undergraduate education, I gained experience in software engineering through internships, developing OpenGL-based medical imaging applications in C++ and iOS applications in Swift. Additionally, I led a software project while at Stratovan, interfacing with customers to develop product roadmaps and make feature-development decisions.
-			</p>
+				<?php include("header.php"); ?>
+				<div class="in_text">
+					<p class="in_text_text">
+						I am currently a Software Application Engineer at Workday. In June 2018, I graduated from the University of California, Davis with a degree in Computer Science. During my undergraduate education, I gained experience in software engineering through internships, developing OpenGL-based medical imaging applications in C++ and iOS applications in Swift. Additionally, I led a software project while at Stratovan, interfacing with customers to develop product roadmaps and make feature-development decisions.
+					</p>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
 	</body>
 	<footer>
 		<?php include("footer.php"); ?>
